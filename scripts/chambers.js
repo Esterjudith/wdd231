e05f6fe9bfaed0d35d4347269fe7a0e09612502a
Judy Hanson <judy.l.hanson@gmail.com>
2024-11-09 23:52:08 -0700

    Home page

---
+++ scripts/chambers.js
@@ -0,0 +1,27 @@
+const currentyear = document.getElementById("currentyear");
+const today = new Date();
+const lastModified = document.getElementById("lastModified");
+const currentPath = window.location.pathname; //This is to highlight which page you are currently in
+const hamButton = document.getElementById("menu");
+const navigation = document.querySelector(".navigation");
+const courses = document.getElementById("courses");
+
+//Select all navigation links
+const navLinks = document.querySelectorAll(".navigation a");
+
+//Toggle for mobile menu
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
+// Loop through each link and add the 'active' class to the matching link
+navLinks.forEach(link => {
+    if(link.getAttribute("href") === currentPath  ) {
+        link.classList.add("active");
+    }
+});
+
+currentyear.innerHTML = today.getFullYear()
+lastModified.textContent = `Last modification: ${document.lastModified}`;
